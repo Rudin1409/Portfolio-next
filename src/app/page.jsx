87c80dd4a9b63713c908660a8aeeb580e7387ba6
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-07-29 06:53:29 +0000

    itu letakkan di bawah comment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,8 +14,8 @@ export default function Home() {
       <AboutSection />
       <StatsSection />
       <ProjectSection />
-      <SocialsSection />
       <ContactSection />
+      <SocialsSection />
     </div>
   );
 }
