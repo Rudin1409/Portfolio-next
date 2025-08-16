62d65257f56d45297a24d539fb3b52cff50856c5
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-08-16 03:48:43 +0000

    saya akan kasih 3 step ya Saya akan menambahkan komponen 3D lanyard dari

--- next.config.js
+++ next.config.js
@@ -16,6 +16,20 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/models/',
+          outputPath: 'static/models/',
+          name: '[name].[hash].[ext]',
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
