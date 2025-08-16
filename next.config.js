bb4ef17ec17906e2e831078b849d14a5de444055
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-08-16 03:54:40 +0000

    Berdasarkan pesan kesalahan yang Anda berikan, `TypeError: Cannot read p

--- next.config.js
+++ next.config.js
@@ -16,20 +16,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(glb|gltf)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/models/',
-          outputPath: 'static/models/',
-          name: '[name].[hash].[ext]',
-        },
-      },
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
