2e439d6dc70abad1f76928b6caa6a2157c59d80e
M Bahrudin <muhammadbahrudin1409@gmail.com>
2025-08-16 03:56:20 +0000

    ya coba kembali

--- next.config.js
+++ next.config.js
@@ -16,6 +16,20 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/images',
+          outputPath: 'static/images/',
+          name: '[name].[hash].[ext]',
+        },
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
